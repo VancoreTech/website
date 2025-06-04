38dc241e6bfd306b968b6297fb8120a5faeb47c8
Christopher Adebayo <christopheradebayo88@gmail.com>
2025-06-04 19:36:04 +0100

    vancore frontend website

---
+++ src/components/ConnectPlatforms.tsx
@@ -0,0 +1,11 @@
+const ConnectPlatforms = () => {
+    return (
+      <div>
+        {/* Your ConnectPlatforms content will go here */}
+        <h2>Connect Platforms</h2>
+        <p>This section is under construction.</p>
+      </div>
+    );
+  };
+  
+  export default ConnectPlatforms;
